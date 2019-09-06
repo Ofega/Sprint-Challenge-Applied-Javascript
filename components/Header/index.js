9ad3b2dd87b74856afceb6eb3620255877a90e8c
ofega <fegaeze@gmail.com>
2019-09-06 04:48:57 +0100

    added textcontent to header dom elements

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,10 @@ function Header() {
     const title = document.createElement('h1');
     const temp = document.createElement('span');
 
+    date.textContent = 'MARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98'
+
     header.appendChild(date);
     header.appendChild(title);
     header.appendChild(temp);
